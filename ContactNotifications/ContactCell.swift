9f6f3ecb6c6be8a04a86d8cf61b29f5d1d44e82c
Neil Sood <soodorama@gmail.com>
2018-11-11 23:45:29 -0800

    custom cell set up

--- ContactNotifications/ContactCell.swift
+++ ContactNotifications/ContactCell.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class ContactCell: UITableViewCell {
     
+    @IBOutlet weak var selectionButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBAction func selectionPressed(_ sender: UIButton) {
+        print("Selected")
+        if sender.backgroundImage(for: .normal) == UIImage(named: "empty_selection") {
+            // switch to full selection
+            // add to selected array
+            sender.setBackgroundImage(UIImage(named: "full_selection"), for: .normal)
+        }
+        else {
+            // switch to empty selection
+            // remove from selected array
+            sender.setBackgroundImage(UIImage(named: "empty_selection"), for: .normal)
+        }
+    }
 }
