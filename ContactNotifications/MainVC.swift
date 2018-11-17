30f71700e3a43e477505ac5fadb1041aa1a5d30c
Neil Sood <soodorama@gmail.com>
2018-11-17 00:58:39 -0800

    populate contacts

--- ContactNotifications/MainVC.swift
+++ ContactNotifications/MainVC.swift
@@ -10,10 +10,35 @@ import UIKit
 import Contacts
 
 class MainVC: UIViewController {
+    
+    var contacts: [CNContact] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getContacts()
+        
+        
+    }
+    
+    func getContacts() {
+        
+        let fetchRequest = CNContactFetchRequest(keysToFetch: [CNContactGivenNameKey as CNKeyDescriptor, CNContactFamilyNameKey as CNKeyDescriptor, CNContactMiddleNameKey as CNKeyDescriptor, CNContactEmailAddressesKey as CNKeyDescriptor,CNContactPhoneNumbersKey as CNKeyDescriptor])
+        
+        fetchRequest.sortOrder = CNContactSortOrder.userDefault
+        
+        let store = CNContactStore()
+        
+        do {
+            try store.enumerateContacts(with: fetchRequest, usingBlock: { (contact, stop) -> Void in
+                print(contact.phoneNumbers.first?.value ?? "no")
+                self.contacts.append(contact)
+                
+            })
+        }
+        catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
     
 }
